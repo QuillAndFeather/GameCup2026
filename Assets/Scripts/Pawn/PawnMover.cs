6b426f551a8819d46d350bfcbf2256d1ff778d87
QuillAndFeather <charlessilbergleith@gmail.com>
2026-09-18 22:52:04 -0700

    adds switching for sprites
    in the movement when sprites are added what ever direction is the player moving the sprite faces that direction

--- Assets/Scripts/Pawn/PawnMover.cs
+++ Assets/Scripts/Pawn/PawnMover.cs
@@ -2,10 +2,45 @@ using UnityEngine;
 
 public class PawnMover : Mover
 {
+    [Header("sprite renderer")]
+    public SpriteRenderer spriteRenderer;
+    public Sprite up, down, horzonatal,idel;
+
     public override void Move(Vector2 direction, float speed)
     {
         // Simple positional movement. Use Rigidbody2D if you want physics-based motion.
         Vector3 displacement = new Vector3(direction.x, direction.y,0f).normalized * speed * Time.deltaTime;
         transform.position += displacement;
+
+        if(up != null && down != null && horzonatal != null && idel != null)
+        {
+
+            if (displacement.x == 0 && displacement.y == 0)
+            {
+                // idel
+            }
+            else
+            {
+                if (displacement.x > 0)
+                {
+                    spriteRenderer.sprite = horzonatal;
+                    spriteRenderer.flipX = false;
+                }
+                else if (displacement.x < 0)
+                {
+                    spriteRenderer.sprite = horzonatal;
+                    spriteRenderer.flipX = true;
+                }
+                if (displacement.y > 0)
+                {
+                    spriteRenderer.sprite = up;
+                }
+                else if (displacement.y < 0)
+                {
+                    spriteRenderer.sprite = down;
+                }
+
+            }
+        }
     }
 }
