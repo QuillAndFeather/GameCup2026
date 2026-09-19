5dd4a2b173e4cca13e857a7d14e34c7d379188b9
Aiden S. <ASchagren@gmail.com>
2026-09-19 11:35:15 -0700

    Player Animations

--- Assets/Scripts/Pawn/PawnMover.cs
+++ Assets/Scripts/Pawn/PawnMover.cs
@@ -6,41 +6,42 @@ public class PawnMover : Mover
     public SpriteRenderer spriteRenderer;
     public Sprite up, down, horzonatal,idel;
 
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>(); //Grab the sprite rendered component
+    }
+
     public override void Move(Vector2 direction, float speed)
     {
         // Simple positional movement. Use Rigidbody2D if you want physics-based motion.
         Vector3 displacement = new Vector3(direction.x, direction.y,0f).normalized * speed * Time.deltaTime;
         transform.position += displacement;
 
-        if(up != null && down != null && horzonatal != null && idel != null)
+        if (displacement.x == 0 && displacement.y == 0)
         {
-
-            if (displacement.x == 0 && displacement.y == 0)
+            // idel
+        }
+        else
+        {
+            if (direction.x > 0)
             {
-                // idel
+                //spriteRenderer.sprite = horzonatal;
+                spriteRenderer.flipX = false;
             }
-            else
+            else if (direction.x < 0)
             {
-                if (displacement.x > 0)
-                {
-                    spriteRenderer.sprite = horzonatal;
-                    spriteRenderer.flipX = false;
-                }
-                else if (displacement.x < 0)
-                {
-                    spriteRenderer.sprite = horzonatal;
-                    spriteRenderer.flipX = true;
-                }
-                if (displacement.y > 0)
-                {
-                    spriteRenderer.sprite = up;
-                }
-                else if (displacement.y < 0)
-                {
-                    spriteRenderer.sprite = down;
-                }
-
+                //spriteRenderer.sprite = horzonatal;
+                spriteRenderer.flipX = true;
             }
+            if (displacement.y > 0)
+            {
+                //spriteRenderer.sprite = up;
+            }
+            else if (displacement.y < 0)
+            {
+                //spriteRenderer.sprite = down;
+            }
+
         }
     }
 }
