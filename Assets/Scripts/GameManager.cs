3af22ce61b1bfd004e7a5a0f03e162b53dceae20
Hung-yo <HNGUYEN89484@uat.edu>
2026-09-19 06:29:46 -0700

    Initial Room Logic
    Created a game manager and RoomTesting scene

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    public GameObject player;
+    public List<Room> rooms = new();
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/Room.cs
+++ Assets/Scripts/Room.cs
@@ -3,16 +3,39 @@ using System.Collections.Generic;
 
 public class Room : MonoBehaviour
 {
-    public List<SpawnObject> spawns;
+    public List<SpawnObject> objectSpawns = new();
+    public GameObject playerSpawn;
+    public GameObject milkManSpawn;
+    public GameManager gameManager;
+    public GameObject milkManPrefab;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        gameManager = GameManager.instance;
+        gameManager.rooms.Add(this);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void StartRoom(GameObject taskPrefab)
+    {
+        gameManager.player.transform.position = playerSpawn.transform.position; // teleport player to room
+        SpawnObject(taskPrefab); // spawn task object in room
+        SpawnMilkMan(); // spawn choco milk man in room
+    }
+
+    private void SpawnObject(GameObject objectToSpawn)
+    {
+        int spawn = Random.Range(0, objectSpawns.Count); // pick a random spawn location
+        Instantiate(objectToSpawn, objectSpawns[spawn].transform.position, Quaternion.identity); // spawn the object at that location
+    }
+
+    private void SpawnMilkMan()
+    {
+        Instantiate(milkManPrefab, milkManSpawn.transform.position, Quaternion.identity);
     }
 }
