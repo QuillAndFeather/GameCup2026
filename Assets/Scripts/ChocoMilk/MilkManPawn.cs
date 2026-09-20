9f2e754f5fe0d69b2ebbcc5d0cf496fef917966b
Hung-yo <HNGUYEN89484@uat.edu>
2026-09-19 18:24:22 -0700

    Speed up milk man in last second

--- Assets/Scripts/ChocoMilk/MilkManPawn.cs
+++ Assets/Scripts/ChocoMilk/MilkManPawn.cs
@@ -2,30 +2,37 @@ using UnityEngine;
 
 public class MilkManPawn : MonoBehaviour
 {
-    private RoomManager roomManager;
+    private GameManager gameManager;
     public PlayerPawn playerPawn;
     public Vector2 movementInput;
+    private float maxTimer = 10;
+    private float currentTimer;
 
     public float speed;
 
     void Start()
     {
-        roomManager = RoomManager.instance;
-        playerPawn = roomManager.playerPawn;
+        gameManager = GameManager.instance;
+        playerPawn = gameManager.playerPawn;
+        currentTimer = maxTimer;
     }
 
     void Update()
     {
       
         MoveTo(playerPawn.transform.position);
-
+        currentTimer -= Time.deltaTime;
+        if (currentTimer < 1)
+        {
+            speed += .05f;
+        }
     }
 
     private void MoveTo(Vector2 position)
     {
-      //todo: make the milk man reach the player after exactly 10 seconds
       //  Vector3 move = new Vector3(position.x, position.y, 0).normalized * speed * Time.deltaTime;
         //transform.position += move;
+
 
         transform.position = Vector2.MoveTowards(transform.position, position, speed * Time.deltaTime);
     }
--- Assets/Scripts/Room/Room.cs
+++ Assets/Scripts/Room/Room.cs
@@ -10,16 +10,18 @@ public class Room : MonoBehaviour
     public List<MilkManSpawnpoint> milkManSpawns = new();
     public List<GameObject> spawnedObjects = new();
     private RoomManager roomManager;
+    private GameManager gameManager;
     private GameObject playerObject;
     private GameObject milkManObject;
     private GameObject milkManPrefab;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        gameManager = GameManager.instance;
         roomManager = RoomManager.instance;
-        roomManager.rooms.Add(this.gameObject);
-        playerObject = roomManager.playerPawn.gameObject;
-        milkManPrefab = roomManager.milkManPrefab;
+        gameManager.rooms.Add(this);
+        playerObject = gameManager.playerPawn.gameObject;
+        milkManPrefab = gameManager.milkManPrefab;
     }
 
     // Update is called once per frame
