c9f5bfeebb9fbbe61c5aff21baee9fa11bb9cdb7
Hung-yo <HNGUYEN89484@uat.edu>
2026-09-20 07:17:53 -0700

    Fix bullet direction to always right

--- Assets/Scripts/Wall Shooting/Shooter.cs
+++ Assets/Scripts/Wall Shooting/Shooter.cs
@@ -26,11 +26,7 @@ public class Shooter : MonoBehaviour
     {
         if (currentCooldown == 0f)
         {
-            // spawn a bullet pointed towards the mouse cursor
-            Vector3 mPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Vector2 mDirection = (mPosition - transform.position);
-            float mAngle = Mathf.Atan2(mDirection.y, mDirection.x) * Mathf.Rad2Deg;
-            Instantiate(bulletPrefab, transform.position, Quaternion.Euler(0f, 0f, mAngle));
+            Instantiate(bulletPrefab, transform.position, Quaternion.identity);
             currentCooldown = maxCooldown;
         }
     }
